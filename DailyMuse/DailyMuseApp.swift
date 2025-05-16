558fed13f218e7a03f008a143552c9fd3b7a8d99
Johnastc2002 <johnastc2002@yahoo.com.hk>
2025-05-16 15:55:12 +0100

    Initial Commit

---
+++ DailyMuse/DailyMuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMuseApp.swift
+//  DailyMuse
+//
+//  Created by tough on 16/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyMuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyMuseTests/DailyMuseTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMuseTests.swift
+//  DailyMuseTests
+//
+//  Created by tough on 16/5/2025.
+//
+
+import Testing
+@testable import DailyMuse
+
+struct DailyMuseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
